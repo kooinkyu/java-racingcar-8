e9f2739db4f8d7f4826921d2d3051d20ac08c8a5
joby <rndlsrb7@gmail.com>
2025-10-26 20:56:05 +0900

    feat: Application 최종 호출 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import racingcar.domain.Game;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        String carNames = inputView.readCarNames();
+        int racingCount = inputView.readRacingCount();
+
+        Game racingGame = new Game(carNames);
+
+        outputView.printResultMessage();
+
+        for (int i = 0; i < racingCount; i++) {
+            racingGame.race();
+            outputView.printRacingResult(racingGame.getCars());
+        }
+
+        outputView.printWinners(racingGame.getWinners());
     }
 }
