08ec81cc2153075bce7cb215a09437c7faa447f2
joby <rndlsrb7@gmail.com>
2025-10-26 20:38:04 +0900

    feat: 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,31 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import java.util.List;
+
 public class OutputView {
+    private static final String RESULT_MESSAGE = "\n실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String POSITION_SYMBOL = "-";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+    }
+
+    public void printRacingResult(List<Car> cars) {
+        for (Car car : cars) {
+            printCarPosition(car);
+        }
+        System.out.println();
+    }
+
+    private void printCarPosition(Car car) {
+        System.out.println(car.getName() + NAME_POSITION_SEPARATOR +
+                POSITION_SYMBOL.repeat(car.getPosition()));
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.println(WINNER_MESSAGE + String.join(", ", winners));
+    }
 }
